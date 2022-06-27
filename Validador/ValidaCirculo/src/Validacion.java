28f345033706cedf9c9c027cc4ee1d9ab717ecd3
Molotovich <molotovich@gmail.com>
2022-06-27 10:22:09 -0500

    Se agrega un programa de un alumno

---
+++ Validador/ValidaCirculo/src/Validacion.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+//Elaborado por Isaac Bonfil Zetina
+public class Validacion {
+    public static void main(String[] args) {
+
+        // Metodo de entrada
+        Scanner sc = new Scanner(System.in);
+
+        // Crear y Asignar variables
+        Double pi = 3.1416, area;
+
+        // Solicitamos los caracteres
+        System.out.println("Por favor ingrese el radio");
+
+        // Creamos una variable donde se almacene nuestro cantidad para despues
+        // convertirlo de
+        // String a double
+        String sradio = sc.nextLine();
+
+        // Creamos una variable para inicializar el ciclo
+        int index = 1;
+        while (index == 1) {
+
+            // Creamos una condicional para validad los caracteres que se ingresan
+            // Si la variable recibe un string procede a ejecutar la oparacion
+            if (sradio.matches("\\d{1,5}")) {
+                Double radio = Double.parseDouble(sradio);
+                radio = Math.pow(radio, 2);
+                area = radio * pi;
+                System.out.println("El area es " + area + " c2");
+
+                // Break funciona para cerrar un ciclo
+                break;
+
+                // Si no, los caracteres no son validos se vuelve a preguntar
+            } else {
+                System.out.println("Por favor ingrese un caracter valido");
+                sradio = sc.nextLine();
+
+            }
+        }
+    }
+}
