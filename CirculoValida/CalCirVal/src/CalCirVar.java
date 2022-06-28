bf9825bf0630a673e308301a9351b733e86207fa
Molotovich <molotovich@gmail.com>
2022-06-27 20:09:11 -0500

    Programa de calculo del circulo con validación

---
+++ CirculoValida/CalCirVal/src/CalCirVar.java
@@ -0,0 +1,34 @@
+
+/*Importamos Libreria Scanner para poder leer los datos que introducirá el usuario */
+import java.util.Scanner;
+
+public class CalCirVar {
+	public static void main(String[] args) {
+		// creacion del objeto scanner para la lectura
+		Scanner in = new Scanner(System.in);
+		// Declaración de Variables
+		double pi;
+		double radioc;
+		String radio;
+		Boolean bandera; //valor que usaremos para indicr a nuestro bucle que es hora de salir
+		/*
+		 * Este programa calcula el área de un circulo,
+		 * pregunta el valor del radio y valida que sean numeros
+		 */
+		do { //utilizamos el bucle DO {} WHILE (condición)
+			System.out.println("Escribe el valor para Radio");
+			radio = in.nextLine(); // Capturamos entrada de usuario
+			if (radio.matches("\\d{1,5}")) { //usamos una expresión regular para validar el dato de entrada
+				radioc = Double.parseDouble(radio); //convertimos de string a double
+				pi = 3.1416; //asignamos valor a pi
+				System.out.println("El área del circulo es " + pi * Math.pow(radioc,2) + " centimetros cuadrados");
+				bandera = false;
+			} else {
+				System.out.println("Valor no valido, vuelva a intentar");
+				bandera = true;
+			}
+		} while (Boolean.TRUE.equals(bandera));
+		in.close();
+	}
+
+}
